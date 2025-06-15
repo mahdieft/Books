5ae6b9f6136e99f392ad614e83c43df38f8cfd87
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-15 10:45:11 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 01: Programming Basics and Strings - example 1

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 01: Programming Basics and Strings/01. Examples/example_01.py
@@ -0,0 +1,21 @@
+"Hello, World. You will never see this"
+
+"This text won't really do anything"
+
+print("Hello, World!")
+
+print('This is a string using a single quote!')
+
+print("This is a string using a double quote!")
+
+print("""This string has three quotes!
+      Look at what it can do!""")
+
+print("I said, \"Don't do it\"")
+
+print('''Roses are red
+Violets are blue
+I just printed multiples lines
+And you did too!''')
+
+print("Roses are red \nViolets are blue \nI just printed multiple lines \nAnd you did too")
