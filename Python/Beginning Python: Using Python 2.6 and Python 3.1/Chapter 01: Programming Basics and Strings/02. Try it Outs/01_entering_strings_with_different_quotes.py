31f09a7ddad66bfdbc3e4d1b926d8b42e374d8e6
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-16 12:06:43 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 01: Programming Basics and Strings - try it out 1

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 01: Programming Basics and Strings/02. Try it Outs/01_entering_strings_with_different_quotes.py
@@ -0,0 +1,9 @@
+"This is a string using a double quote"
+
+'This is a string using a single quote'
+
+"""This string has triple double quotes.
+   Look at what it can do."""
+
+'''This string has triple single quotes.
+   It can span multiple lines.'''
