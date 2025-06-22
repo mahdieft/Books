a0e51229b465e3c5405a3c1c373c642296ebd3fb
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-21 22:48:54 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - example 3

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/01. Examples/example_03.py
@@ -0,0 +1,4 @@
+a = ("first", "second", "third")
+print("The first element of the tuple is %s" % a[0])
+print("The second element of the tuple is %s" % a[1])
+print("The third element of the tuple is %s" % a[2])
