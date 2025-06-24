f082451c0ac9e49f619b8957d9df350718b2cea8
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-24 12:42:36 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - example 11

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/01. Examples/example_11.py
@@ -0,0 +1,7 @@
+last_names = ["Douglass", "Jefferson", "Williams", "Frank", "Thomas"]
+by_letter = {}
+by_letter[last_names[0][0]] = last_names[0]
+by_letter[last_names[1][0]] = last_names[1]
+by_letter[last_names[2][0]] = last_names[2]
+by_letter[last_names[3][0]] = last_names[3]
+by_letter[last_names[4][0]] = last_names[4]
