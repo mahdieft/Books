3766c7de04aa8cd52c55466ec2591ee6dd95c499
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-24 12:42:15 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - example 10

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/01. Examples/example_10.py
@@ -0,0 +1,12 @@
+last_names = [ "Douglass", "Jefferson", "Williams", "Frank", "Thomas" ]
+print("%s" % last_names[0])
+print("%s" % last_names[0][0])
+print("%s" % last_names[1])
+print("%s" % last_names[1][0])
+print("%s" % last_names[2])
+
+print("%s" % last_names[2][0])
+print("%s" % last_names[3])
+print("%s" % last_names[3][0])
+print("%s" % last_names[4])
+print("%s" % last_names[4][0])
