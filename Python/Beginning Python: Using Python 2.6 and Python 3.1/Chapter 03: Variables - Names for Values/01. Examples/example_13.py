e197b3602853ca5f5b701438ea44c02a71e3d93b
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-24 12:43:11 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - example 13

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/01. Examples/example_13.py
@@ -0,0 +1,9 @@
+living_room = ("rug", "table", "chair", "TV", "dustbin", "shelf")
+apartment = []
+apartment.append(living_room)
+print(apartment)
+      
+apartment = []
+apartment.extend(living_room)
+print(apartment)
+      
