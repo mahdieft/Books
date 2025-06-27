59e7a62a11a958592529abe7754fedc95b083d1d
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-27 18:55:27 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - Try it Out 5

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/02. Try it Outs/05_viewing_the_elements_of_a_list.py
@@ -0,0 +1,12 @@
+breakfast = [ "coffee", "tea", "toast", "egg" ]
+count = 0
+print("Today's breakfast is %s" % breakfast[count])
+
+count = 1
+print("Today's breakfast is %s" % breakfast[count])
+
+count = 2
+print("Today's breakfast is %s" % breakfast[count])
+
+count = 3
+print("Today's breakfast is %s" % breakfast[count])
