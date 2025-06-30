7c86f9f63e72dc8e11d1ad3c56d3e5706215f803
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-30 17:11:00 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - Try it Out 8

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/02. Try it Outs/08_slicing_sequences.py
@@ -0,0 +1,11 @@
+slice_me = ("The", "next", "time", "we", "meet", "drinks", "are", "on", "me") 
+sliced_tuple = slice_me[5:9]
+print(sliced_tuple)
+
+slice_this_list = ["The", "next", "time", "we", "meet", "drinks", "are", "on", "me"]
+sliced_list = slice_this_list[5:9]
+print(sliced_list)
+
+slice_this_string = "The next time we meet, drinks are on me"
+sliced_string = slice_this_string[5:9]
+print(sliced_string)
