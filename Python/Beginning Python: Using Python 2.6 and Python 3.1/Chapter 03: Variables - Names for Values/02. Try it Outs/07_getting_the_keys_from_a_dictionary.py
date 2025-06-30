149e7aab467f776faec8b7a39b71eb7c26efad82
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-06-30 17:10:51 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - Try it Out 7

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/02. Try it Outs/07_getting_the_keys_from_a_dictionary.py
@@ -0,0 +1,11 @@
+menu_specials = {
+    "breakfast": "sausage and eggs",
+    "lunch": "split pea soup and garlic bread",
+    "dinner": "2 hot dogs and onion rings",
+}
+
+hungry = menu_specials.keys()
+print(list(hungry))
+
+starving = menu_specials.values()
+print(list(starving))
