6af51ac47e5c95b09a487e899d4fb5a98f2cc2e5
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-07-01 10:16:59 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - Exercise 6

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/03. Exercises/exercise_06.py
@@ -0,0 +1,9 @@
+milk_expiration = (9, 12, 2025)
+milk_carton = {
+    'expiration_date': milk_expiration,
+    'fl_oz': 12,
+    'Cost': 12,
+    'brand_name': 'Dairy Farm',
+}
+
+print(list(milk_carton.values()))
