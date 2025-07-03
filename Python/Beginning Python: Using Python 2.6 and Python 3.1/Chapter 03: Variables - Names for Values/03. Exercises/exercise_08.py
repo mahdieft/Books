0abffc16e4eb0fcf24eb9f7cd6081db557cfe8ff
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-07-03 11:12:11 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 03: Variables - Names for Values - Exercise 8

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 03: Variables - Names for Values/03. Exercises/exercise_08.py
@@ -0,0 +1,7 @@
+dairy_section = ['Milk', 'Cheese', 'Butter', 'Yogurt']
+cheeses = ["cheddar", "american", "mozzarella"]
+
+dairy_section.append(cheeses)
+
+print(dairy_section)
+print(dairy_section.pop())
