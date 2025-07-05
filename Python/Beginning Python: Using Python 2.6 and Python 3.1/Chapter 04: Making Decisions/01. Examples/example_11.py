7be594247e74ad67e7949b6f2a93ae4529b1e13c
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-07-05 10:23:18 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 04: Making Decisions - example 11

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 04: Making Decisions/01. Examples/example_11.py
@@ -0,0 +1,7 @@
+milk_price = 1.50
+if milk_price < 1.25:
+    print("Buy two cartons of milk, they’re on sale")
+elif milk_price < 2.00:
+    print("Buy one carton of milk, prices are normal")
+elif milk_price > 2.00:
+    print("Go somewhere else!  Milk costs too much here")
