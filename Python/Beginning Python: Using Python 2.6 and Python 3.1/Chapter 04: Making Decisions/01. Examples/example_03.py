28e5fb3394248b858f45bd2a4d4728dbb96289f4
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-07-04 20:16:11 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 04: Making Decisions - example 3

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 04: Making Decisions/01. Examples/example_03.py
@@ -0,0 +1,6 @@
+tuesday_breakfast_sold = {"pancakes":10, "french toast": 4, "bagels":32, "omelets":12, "eggs and sausages":13}
+wednesday_breakfast_sold = {"pancakes":8, "french toast": 5, "bagels":22, "omelets":16, "eggs and sausages":22}
+print(tuesday_breakfast_sold == wednesday_breakfast_sold)
+
+thursday_breakfast_sold = {"pancakes":10, "french toast": 4, "bagels":32, "omelets":12, "eggs and sausages":13}
+print(tuesday_breakfast_sold == thursday_breakfast_sold)
