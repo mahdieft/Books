068ff6812d2065e4fa9aa481e024f3b4004a0a19
Mahdi Eftekhari Moghaddam <mahdi.eft@gmail.com>
2025-07-04 20:16:03 -0400

    Python - Beginning Python: Using Python 2.6 and Python 3.1 - Chapter 04: Making Decisions - example 2

---
+++ Python/Beginning Python: Using Python 2.6 and Python 3.1/Chapter 04: Making Decisions/01. Examples/example_02.py
@@ -0,0 +1,8 @@
+apples = ["Mackintosh", "Golden Delicious", "Fuji", "Mitsu"]
+apple_trees = ["Golden Delicious", "Fuji", "Mitsu", "Mackintosh"]
+
+print(apples == apple_trees)
+      
+apple_trees = ["Mackintosh", "Golden Delicious", "Fuji", "Mitsu"]
+print(apples == apple_trees)
+      
